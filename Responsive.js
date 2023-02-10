69ddaf8474809b1dd1a5aee938ffffdde793c3cd
Gauthan Bairi <gautham0647@gmail.com>
2023-02-10 13:49:56 +0530

    css hlalf part done

--- Responsive.js
+++ Responsive.js
@@ -3,13 +3,13 @@
 
 const centerDiv = document.getElementById('centerDiv');
 
-fetch("https://jsonplaceholder.typicode.com/photos?_limit=10 ")
+fetch("https://jsonplaceholder.typicode.com/photos?_limit=20 ")
 .then( (res)=>res.json())
 .then ((jsondata) => {
 jsondata.map((val)=>{
  key = val.id;
  let img = document.createElement("img");
- img.src= `https://picsum.photos/200/300?random=${val.id}`;
+ img.src= `https://picsum.photos/200/350?random=${val.id}`;
  centerDiv.appendChild(img);
  console.log(val.url)
 
